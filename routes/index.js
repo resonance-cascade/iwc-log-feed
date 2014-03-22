b5346868c024bc3ce7d1f876d8a8fc54bcec475d
Bret Comnes <bcomnes@gmail.com>
2014-03-21 18:20:40 -0700

    Starting to check if updates are needed upon request.

--- routes/index.js
+++ routes/index.js
@@ -7,10 +7,20 @@ exports.index = function(req, res){
   res.render('index', { title: 'IndieWebCamp IRC Atom Feed' });
 };
 
+
+/*
+ * GET Atom Feed
+ */
+
 exports.atom = function(req, res){
   res.contentType('application/atom+xml');
   res.render('atom');
 };
+
+
+/*
+ * GET HTML test
+ */
 
 exports.htmltest =function(req,res) {
   res.render('htmltest',  { title: 'HTML output test' });
